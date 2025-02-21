4d58ad99db7a7f9be9e1c2c0e5493856c82ad2cb
Rafael <monteiro.c.rafaell@gmail.com>
2025-02-21 17:00:17 -0300

    Update login.js

--- login.js
+++ login.js
@@ -26,11 +26,11 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-          const response = await fetch(`${API_URL}/login`, { 
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ username, password }),
-          });
+        const response = await fetch(`${API_URL}/api/auth/login`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        });
 
           if (!response.ok) {
               const errorData = await response.json().catch(() => ({ message: "Erro desconhecido" }));
