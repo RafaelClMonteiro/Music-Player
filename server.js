4196b1e10c188e9484c93c2a641a45cddf18dbb9
Rafael <monteiro.c.rafaell@gmail.com>
2025-02-25 14:35:58 -0300

    Update

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-console.log("Mongo URI:", process.env.MONGO_URI);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("./player-music-backend/routes/AuthRoutes.js");
 const path = require('path');
+require("dotenv").config();
+
+const authRoutes = require("./player-music-backend/routes/AuthRoutes.js");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors({
   methods: "GET,POST",
   allowedHeaders: "Content-Type"
 }));
+
 app.use(express.json());
 
 // Conexão com o MongoDB
@@ -21,23 +23,17 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
 
+app.use(express.static(__dirname));
+app.use("/styles", express.static(path.join(__dirname, "styles"))); 
+
 // Roteamento
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname)));
 
 // Roteamento para páginas HTML
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "register.html"));
-});
-
-app.get("/musicPlayer", (req, res) => {
-  res.sendFile(path.join(__dirname, "musicPlayer.html"));
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "login.html")));
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "register.html")));
+app.get("/musicPlayer", (req, res) => res.sendFile(path.join(__dirname, "musicPlayer.html")));
 
 // Inicia o servidor
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
