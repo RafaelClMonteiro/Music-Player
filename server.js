7d78e271c26c6836ba276600de8cba929e0ddb60
Rafael <monteiro.c.rafaell@gmail.com>
2025-02-25 14:59:49 -0300

    Update

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// Configuração de CORS
 app.use(cors({
   origin: ["http://localhost:5000", "https://music-player-kohl-alpha.vercel.app"],
   methods: ["GET", "POST"],
@@ -19,23 +18,18 @@ app.use(cors({
 
 app.use(express.json());
 
-// Conexão com MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
 
-// Servir arquivos estáticos
-app.use(express.static(path.join(__dirname))); // Serve arquivos .js e .html na raiz
-app.use("/styles", express.static(path.join(__dirname, "styles"))); // Serve CSS
-app.use("/assets", express.static(path.join(__dirname, "assets"))); // Serve imagens
+app.use(express.static(path.join(__dirname))); 
+app.use("/styles", express.static(path.join(__dirname, "styles"))); 
+app.use("/assets", express.static(path.join(__dirname, "assets"))); 
 
-// Rotas de API
 app.use("/api/auth", authRoutes);
 
-// Rotas HTML
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "login.html")));
 app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "register.html")));
 app.get("/musicPlayer", (req, res) => res.sendFile(path.join(__dirname, "musicPlayer.html")));
 
-// Inicia o servidor
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
