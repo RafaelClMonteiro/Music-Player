5b1c64c48a93754f904d3ca159c718a0e1c0a7da
Rafael <monteiro.c.rafaell@gmail.com>
2025-02-28 14:08:43 -0300

    Update

--- server.js
+++ server.js
@@ -9,10 +9,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '.')));
+app.use(express.static(path.join(__dirname, '.'), {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    } else if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    } else if (path.endsWith('.jpg') || path.endsWith('.png') || path.endsWith('.jpeg')) {
+      res.setHeader('Content-Type', 'image/jpeg');
+    }
+  }
+}));
 
 app.use(cors({
-  origin: ['http://localhost:5000', 'https://music-player-8k42a41l.vercel.app'],
+  origin: ['http://localhost:5000', 'https://music-player-kohl-alpha.vercel.app'],
   credentials: true
 }));
 
@@ -32,12 +42,31 @@ app.get('/musicPlayer.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'musicPlayer.html'));
 });
 
-
-app.get("/", (req, res) => {
-  res.redirect("/login.html");
+app.get('/styles/loginStyle.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'styles', 'loginStyle.css'));
 });
 
-app.use("/auth", authRoutes);
+app.get('/login.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.js'));
+});
+
+app.get('/register.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'register.js'));
+});
+
+app.get('/playerScript.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'playerScript.js'));
+});
+
+app.get('/LoginBg.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname, 'LoginBg.jpg'));
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/login.html"); 
+});
+
+app.use("/auth", authRoutes); 
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -51,4 +80,4 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-module.exports = app;
+module.exports = app; 
